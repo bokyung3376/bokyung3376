427be1258c0c00e350e636e3d6876ee410a3552c
bokyung3376 <bokyung3376@snu.ac.kr>
2025-01-18 06:39:23 +0000

    문구수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,8 +21,7 @@ function HomepageHeader() {
                     <Link
                         className="button button--secondary button--lg"
                         to="/docs/intro">
-                        {/* TODO: fix this line */}
-                        How to Join - 1min ⏱️
+                        싸움잘해?
                     </Link>
                 </div>
             </div>
